a31889301d96770490e0c96b1f1b45f7a52991b2
Juan Jose Sanchez Silos <sanchez.silos@gmail.com>
2012-09-30 13:23:21 +0200

    signin wrong OK

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   
   #get "users/new"
   resources :users
+  resource :sessions, only: [:new, :create, :destroy]
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
@@ -9,6 +10,8 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
