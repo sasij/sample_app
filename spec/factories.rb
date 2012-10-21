8eef205314ab0b0b1b2cd030c447ba65ab0f6a34
Juan Jose Sanchez Silos <sanchez.silos@gmail.com>
2012-10-21 20:27:13 +0200

    starting 9.4

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 	factory :user do
-		name 		"Juan Jose"
-		email 		"jj@uco.es"
+		sequence(:name)  { |n| "Person #{n}" }
+    	sequence(:email) { |n| "person_#{n}@example.com"} 
 		password 	"foobar"
 		password_confirmation "foobar"
 	end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,9 +27,12 @@ describe "AuthenticationPages" do
   		end
 
   		it {should have_selector('title', text: user.name)}
+
+      it { should have_link('Users', href: users_path)}
   		it {should have_link('Profile', href: user_path(user))}
       it {should have_link('Settings', href: edit_user_path(user))}
   		it {should have_link('Sign out', href: signout_path)}
+
   		it {should_not have_link('Sign in', href: signin_path)}
   	end
   end
@@ -62,6 +65,10 @@ describe "AuthenticationPages" do
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
+        end
+        describe "visiting the user index" do
+          before {visit users_path}
+          it{ should have_selector('title', text: 'Sign in')}
         end
       end
     end
